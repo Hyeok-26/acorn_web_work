04d5d1c4cf9ea60071dc1af47cb1f8e5b3a1963a
Noh Hyeokcheon <makdeng0132@gmail.com>
2025-02-17 18:22:39 +0900

    Step04

---
+++ Spring00_test/src/main/java/com/example/spring00/Spring00TestApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring00TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring00TestApplication.class, args);
+	}
+
+}
---
+++ Spring00_test/src/test/java/com/example/spring00/Spring00TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring00TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
